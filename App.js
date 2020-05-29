d26bd1ba45a8996029c4eb05994490654295817c
Engincan Veske <enginveske@gmail.com>
2020-05-29 10:40:08 +0300

    login and auth flow has been configured

--- App.js
+++ App.js
@@ -1,23 +1,60 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
+import { Button, Alert } from "react-native";
 
 //navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as RootNavigation from "./src/helper/RootNavigation";
 
-// import UserFlatList from './src/components/UserFlatList'
 import HomeScreen from './src/pages/HomeScreen'
 import Register from './src/pages/Register'
+import MainPage from "./src/pages/MainPage";
+import AsyncStorage from '@react-native-community/async-storage';
+
 
 const Stack = createStackNavigator();
 
 export default class App extends Component {
+  state = {
+    isLoggedIn: false
+  }
+  componentDidMount = async () => {
+    const val = await AsyncStorage.getItem("username");
+    if (val !== null)
+      this.setState({ isLoggedIn: true });
+  }
   render() {
     return (
-      <NavigationContainer>
+      <NavigationContainer ref={RootNavigation.navigationRef}>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}/>
+          {
+            this.state.isLoggedIn ? (
+              <Stack.Screen name="MainPage" component={MainPage} options={{
+                headerRight: () => (
+                  <Button
+                    onPress={async () => { await AsyncStorage.clear(); this.setState({ isLoggedIn: false }); RootNavigation.navigate("Home") }}
+                    title="Logout"
+                    color="blue"
+                  />
+                ),
+              }} />
+
+            ) : (
+                <React.Fragment>
+                  <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+                  <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+                  <Stack.Screen name="MainPage" component={MainPage} options={{
+                    headerRight: () => (
+                      <Button
+                        onPress={async () => { await AsyncStorage.clear(); this.setState({ isLoggedIn: false }); RootNavigation.navigate("Home") }}
+                        title="Logout"
+                        color="blue"
+                      />
+                    ),
+                  }} />
+                </React.Fragment>
+              )}
         </Stack.Navigator>
       </NavigationContainer>
     );
