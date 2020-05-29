18eb017c748477e78767021d4d487f0b54aa8a78
oguzhankaymak <oguzhankaymakdev@gmail.com>
2020-05-30 02:20:42 +0300

    Private account page created

--- src/pages/PrivateAccountPage.js
+++ src/pages/PrivateAccountPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import PrivateAccountCompanent from '../components/PrivateAccountCompanent'
+
 const PrivateAccountPage = () => {
     return (
         <View>
-            <Text>Giriş ile burası</Text>
+            <PrivateAccountCompanent/>
         </View>
     )
 }
